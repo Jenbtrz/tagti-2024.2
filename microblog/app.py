337690d0088b1ada50d8730e5d706e8eca369f0d
Jenifer Beatriz <jenifer.beatriz@aluno.ifsertao-pe.edu.br>
2024-10-21 20:59:35 -0300

    Utilização do render template

--- microblog/app.py
+++ microblog/app.py
@@ -1,13 +1,17 @@
 #python -m venv venv
 #.\venv\Scripts\activate
 #pip install flask
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá Mundo!'
+    return render_template("index.html")
+
+@app.route('/')
+def index():
+    return render_template("contato.hmtl")
 
 if __name__ == '__main__':
     app.run()

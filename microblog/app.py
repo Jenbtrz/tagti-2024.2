b0be2dcf74dd1a169d7049fe25e450f3dcf05d12
Jenifer Beatriz <jenifer.beatriz@aluno.ifsertao-pe.edu.br>
2024-11-04 21:15:41 -0300

    Aula 04/11

--- microblog/app.py
+++ microblog/app.py
@@ -9,15 +9,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-<<<<<<< HEAD
-@app.route('/')
-def index():
-    return render_template("contato.hmtl")
-=======
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
->>>>>>> d5477831f052034a1dc9fb8f3a83977194d7fdc1
+
+#Calculadora para somar dois números passados por por parâmentro 
+@app.route("/soma/<int:num1>/<int:num2>")
+def soma(num1,num2): 
+    return f"resultado da soma: {num1+num2}"
+    
+
 
 if __name__ == '__main__':
     app.run()
